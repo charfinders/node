b8a13b5225f3a045a8ffab972cdddeee4de4f95f
Luciano Ramalho <luciano@ramalho.org>
2016-01-06 17:57:59 -0200

    indexes implemented

--- parser.js
+++ parser.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 
+var codeMap = new Map();
+var nameMap = new Map();
+
 function readUCDLines (cb) {
   fs.readFile('UnicodeData.txt', 'ascii', function (err, data) {
     if (err) {
@@ -10,6 +13,28 @@ function readUCDLines (cb) {
   });
 }
 
+function loadIndexes(cb) {
+  readUCDLines(function (err, data) {
+    if (err) {
+      cb(err);
+    } else {
+      data.forEach(function (line) {
+        var parts = line.split(';');
+        if (parts[1] && !parts[1].startsWith('<')) {
+          codeMap.set(parts[0], parts[1]);
+          parts[1].split(' ').forEach(function(word) {
+            nameMap.set(word, (nameMap.get(word) || new Set()).add(parts[0]));
+          });
+        }
+      });
+      cb();
+    }
+  });
+}
+
 module.exports = {
-  readUCDLines: readUCDLines
+  readUCDLines: readUCDLines,
+  loadIndexes: loadIndexes,
+  codeMap: codeMap,
+  nameMap: nameMap,
 };
--- test/parser_test.js
+++ test/parser_test.js
@@ -3,6 +3,7 @@ var parser = require('../parser.js');
 
 describe('Parser', function () {
   describe('#readUCDLines', function () {
+      
     it('should return an array', function (done) {
       parser.readUCDLines(function (err, data) {
         if (err) done(err);
@@ -10,5 +11,48 @@ describe('Parser', function () {
         done();
       });
     });
+    
+    it('should return more than 20K items', function (done) {
+      parser.readUCDLines(function (err, data) {
+        if (err) done(err);
+        data.length.should.be.above(29000);
+        done();
+      });
+    });
+  });
+  describe('#loadIndexes', function (done) {
+
+    it('should add at least 20K items to the codeMap', function (done) {
+      parser.loadIndexes(function (err) {
+        if (err) done(err);
+        parser.codeMap.size.should.be.above(29000);
+        done();
+      });
+    });
+
+    it('codeMap should map "0020" to "SPACE"', function (done) {
+      parser.loadIndexes(function (err) {
+        if (err) done(err);
+        parser.codeMap.get('0020').should.equal('SPACE');
+        done();
+      });
+    });
+
+    it('nameMap should map "SPACE" to set containing "0020"', function (done) {
+      parser.loadIndexes(function (err) {
+        if (err) done(err);
+        parser.nameMap.get('SPACE').has("0020");
+        done();
+      });
+    });
+
+    it('nameMap should map "LATIN" to set with more than one item', function (done) {
+      parser.loadIndexes(function (err) {
+        if (err) done(err);
+        parser.nameMap.get('LATIN').size.should.be.above(1);
+        done();
+      });
+    });
+
   });
 });
